bc9202a5c9f4e5eaf91f808bf7b8f239ad14bc50
petersonbem <petersondaronch@icloud.com>
2025-06-06 17:38:27 -0300

    validate keys for url safety

--- keygen-service/keys/key.go
+++ keygen-service/keys/key.go
@@ -1,27 +1,86 @@
 package keys
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
 
+var (
+	UrlSafePattern = regexp.MustCompile(`^[a-zA-Z0-9_-]*$`)
+)
+
+// ShortKey is a URL-safe 6-byte key
 type ShortKey [6]byte
 
 func (s *ShortKey) Bytes() []byte {
 	return s[:]
 }
 
+// ShortKeyValidationError aggregates errors found
+// during creation of a new short key
+type ShortKeyValidationError struct {
+	Entries []ShortKeyValidationEntry
+}
+
+// ShortKeyValidationEntry describes a validation
+// error in the short key
+type ShortKeyValidationEntry struct {
+	Field string
+	Error error
+}
+
+func (s ShortKeyValidationError) Error() string {
+	return fmt.Sprintf("short key validation errors: %v", s.Entries)
+}
+
+// Valid indicates when errors were found
+func (s *ShortKeyValidationError) Valid() bool {
+	return len(s.Entries) == 0
+}
+
 // NewKeyFromBytes creates a short key from given
-// bytes if it represents a valid content, validation
-// errors otherwise
-func NewKeyFromBytes(content []byte) (*ShortKey, []error) {
-	var shortKey ShortKey
-	errs := make([]error, 0)
+// bytes if it represents a valid content, returns
+// validation an error otherwise
+func NewKeyFromBytes(content []byte) (*ShortKey, error) {
+	var validation ShortKeyValidationError
 
+	validateBytes(content, &validation)
+
+	if !validation.Valid() {
+		return nil, validation
+	}
+
+	shortKey := ShortKey(content)
+	return &shortKey, nil
+}
+
+func validateBytes(content []byte, v *ShortKeyValidationError) {
+	validations := []func(content []byte, v *ShortKeyValidationError){
+		validateBytesSize,
+		validateBytesUrlSafe,
+	}
+
+	for _, f := range validations {
+		f(content, v)
+	}
+}
+
+func validateBytesSize(content []byte, v *ShortKeyValidationError) {
 	if len(content) != len(ShortKey{}) {
-		errs = append(errs, errors.New("invalid key size"))
-	}
+		size := "big"
+		if len(content) < len(ShortKey{}) {
+			size = "small"
+		}
 
-	if len(errs) == 0 {
-		shortKey = ShortKey(content)
+		v.Entries = append(
+			v.Entries, ShortKeyValidationEntry{"key", fmt.Errorf("%s key size", size)})
 	}
+}
 
-	return &shortKey, errs
+func validateBytesUrlSafe(content []byte, v *ShortKeyValidationError) {
+	if !UrlSafePattern.Match(content) {
+		v.Entries = append(
+			v.Entries, ShortKeyValidationEntry{"key", errors.New("not URL safe")})
+	}
 }
--- keygen-service/keys/key_test.go
+++ keygen-service/keys/key_test.go
@@ -1,32 +1,49 @@
 package keys
 
 import (
+	"errors"
+	"slices"
 	"strings"
 	"testing"
 )
 
 func TestNewKeyFromBytes_GivenValidContent(t *testing.T) {
 	content := []byte("abc123")
-	got, errs := NewKeyFromBytes(content)
+	got, err := NewKeyFromBytes(content)
 
-	if len(errs) != 0 {
-		t.Errorf("NewKeyFromBytes(%v) = (%v, %v), want no errors", content, got, errs)
+	if err != nil {
+		t.Errorf("NewKeyFromBytes(%v) = (%v, %v), want no errors", content, got, err)
 	}
 }
 
-func TestNewKeyFromBytes_GivenInvalidSizeContent(t *testing.T) {
-	want := "invalid key size"
-	cases := [][]byte{[]byte("ab1"), []byte("abcd1234")}
+func TestNewKeyFromBytes_GivenInvalidContent(t *testing.T) {
+	cases := []struct {
+		content []byte
+		want    []string
+	}{
+		{[]byte("ab1/"), []string{"small key size", "not URL safe"}},
+		{[]byte("abcd12@4"), []string{"big key size", "not URL safe"}},
+		{[]byte("abcd1234"), []string{"big key size"}}, // single error
+	}
 
 	for _, c := range cases {
-		got, errs := NewKeyFromBytes(c)
+		got, err := NewKeyFromBytes(c.content)
 
-		if len(errs) == 0 {
-			t.Fatalf("NewKeyFromBytes(%v) = (%v, %v), want some errors", c, got, errs)
+		if err == nil {
+			t.Fatalf("NewKeyFromBytes(%v) = (%v, %v), want some errors", c.content, got, err)
 		}
 
-		if !strings.Contains(errs[0].Error(), want) {
-			t.Errorf("NewKeyFromBytes(%v) = (%v, %v), want to contain %v", c, got, errs, want)
+		var validationError ShortKeyValidationError
+		if !errors.As(err, &validationError) {
+			t.Fatalf("NewKeyFromBytes(%v) = (%v, %v), want validation error", c.content, got, err)
+		}
+
+		for _, w := range c.want {
+			if !slices.ContainsFunc(validationError.Entries, func(e ShortKeyValidationEntry) bool {
+				return e.Field == "key" && strings.Contains(e.Error.Error(), w)
+			}) {
+				t.Errorf("NewKeyFromBytes(%v) = (%v, %v), want error containing %v (specifically %v)", c.content, got, err, c.want, w)
+			}
 		}
 	}
 }

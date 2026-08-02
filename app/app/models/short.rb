88c19a954485f2d82fc61e423f9bed01e019f333
petersonbem <petersondaronch@icloud.com>
2026-08-02 18:29:15 -0300

    take note about some not reproducible validation in the short model

--- app/app/models/short.rb
+++ app/app/models/short.rb
@@ -2,5 +2,7 @@
 class Short < ApplicationRecord
   validates :token, :original_url, :expire_at, presence: true
   validates :token, length: { is: 6 }
+
+  # TODO: this can't be validated at the data layer, so consider moving it to specifc forms
   validates :original_url, long_url: true
 end

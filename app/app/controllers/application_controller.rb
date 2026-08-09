27bcc230e4c2d39228a79d0c7dd60c113e63dc19
petersonbem <petersondaronch@icloud.com>
2026-08-09 20:58:52 -0300

    [WIP] fix i18n not being persisted

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
@@ -6,10 +8,21 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def switch_locale(&action)
-    locale = params[:locale]&.to_s || ""
-    locale = I18n.default_locale unless I18n.locale_available?(locale)
+  LOCALE_COOKIE = :locale
 
+  def switch_locale(&action)
+    locale = LocaleResolver.resolve(
+      cookie_locale: cookies[LOCALE_COOKIE],
+      accept_language_header: request.env["HTTP_ACCEPT_LANGUAGE"]
+    )
+
+    persist_locale(locale)
     I18n.with_locale(locale, &action)
+  end
+
+  def persist_locale(locale)
+    return if cookies[LOCALE_COOKIE] == locale
+
+    cookies.permanent[LOCALE_COOKIE] = locale
   end
 end
--- app/config/application.rb
+++ app/config/application.rb
@@ -23,5 +23,8 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = %i[en pt-BR]
+    config.i18n.default_locale = :en
   end
 end
